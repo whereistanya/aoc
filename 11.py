f81ea9e5a3992eb2da3c0bb1d1667fb2cc9f834d
Tanya Reilly <tanya@noidea.dog>
2018-12-15 00:00:15 -0500

    11 part 2.

--- 11.py
+++ 11.py
@@ -10,6 +10,7 @@ class PowerGrid(object):
 		for x in range(0, self.max):
 			for y in range(0, self.max):
 				self.grid[(x, y)] = self.get_power_level(x, y)
+				self.squares[(x, y, 1)] = self.grid[(x, y)]
 
 	def get_power_level(self, x, y):
 		rack_id = x + 10
@@ -43,26 +44,35 @@ class PowerGrid(object):
 	def get_square(self, x, y, size):
 		if (x + size) > self.max or (y + size) > self.max:
 			return None
-		total_power = 0
-		for i in range (0, size):
-			for j in range (0, size):
-				total_power  += self.grid[(x + i, y + j)]
+		if (x, y, size) in self.squares:
+			return self.squares[(x, y, size)]
 
+		total_power = self.get_square(x, y, size - 1)
+		for i in range (0, size - 1):
+			xi = x + i
+			yi = y + i
+			total_power += self.grid[(xi, y + size -1)]
+			total_power += self.grid[(x + size -1, yi)]
+		total_power += self.grid[(x + size -1, y + size -1)]
+		self.squares[(x, y, size)] = total_power
 		return total_power
 
-
-
+"""
 # tests
-#assert PowerGrid(8).get_power_level(3, 5) == 4
-#assert PowerGrid(57).get_power_level(122, 79) == -5
-#assert PowerGrid(39).get_power_level(217, 196) == 0
-#assert PowerGrid(71).get_power_level(101, 153) == 4
-#assert PowerGrid(18).get_highest_square(3) == ((33, 45, 3), 29)
-#assert PowerGrid(42).get_highest_square(3) == ((21, 61, 3), 30)
+assert PowerGrid(8).get_power_level(3, 5) == 4
+assert PowerGrid(57).get_power_level(122, 79) == -5
+assert PowerGrid(39).get_power_level(217, 196) == 0
+assert PowerGrid(71).get_power_level(101, 153) == 4
+assert PowerGrid(18).get_highest_square(3) == ((33, 45, 3), 29)
+assert PowerGrid(42).get_highest_square(3) == ((21, 61, 3), 30)
+assert PowerGrid(18).get_highest_square(17) == ((90,269,16), 113)
+assert PowerGrid(42).get_highest_square(17) == ((232,251,12), 119)
+"""
+
 
 serial = 9306
 grid = PowerGrid(serial)
-print grid.get_highest_square(3)  # Submit without a space
-#grid = PowerGrid(18)
-#print grid.get_highest_square(16)
+
+# Guessing this is a square of 20 or less. Can try bigger ones but it's slow.
+print grid.get_highest_square(20)  # Submit without a space
 
