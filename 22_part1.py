7f133391b04d12868eb1e2b4665d87bf13be1884
Tanya Reilly <tanya@noidea.dog>
2018-12-28 23:20:14 -0500

    22 part 1.

--- 22_part1.py
+++ 22_part1.py
@@ -9,22 +9,30 @@ class Region(object):
   def __init__(self, x, y, depth):
     self.x = x
     self.y = y
+    self.is_target = False
     self.depth = depth
-    self._region_type = "?"
+    self._region_type = -1
     self._erosion_level = -1
     self._geologic_index = -1
     self.xminus1 = None # neighbour
     self.yminus1 = None # neighbour
 
   def region_type(self):
-    if self._region_type == "?":
-      self.erosion_level()
+    if self._region_type >= 0:
+      return self._region_type
+    self.erosion_level()
+    if self._region_type < 0:
+      print "Still -1 after e_l()"
     return self._region_type
 
   def geologic_index(self):
-    if self._geologic_index > 0:
+    if self._geologic_index >= 0:
       return self._geologic_index
-    if self.y == 0:
+    if self.x == 0 and self.y == 0:
+      self._geologic_index = 0
+    elif self.is_target:
+      self._geologic_index = 0
+    elif self.y == 0:
       self._geologic_index = self.x * 16807
     elif self.x == 0:
       self._geologic_index = self.y * 48271
@@ -33,20 +41,11 @@ class Region(object):
     return self._geologic_index
 
   def erosion_level(self):
-    if self._erosion_level > 0:
+    if self._erosion_level >= 0 and self.region_type >= 0:
       return self._erosion_level
     self._erosion_level = (self.geologic_index() + self.depth) % 20183
-    level = self._erosion_level % 3
-    if level == 0:
-      self._region_type = "."  # rocky
-    elif level == 1:
-      self._region_type = "="  # wet
-    elif level == 2:
-      self._region_type = "|"  # narrow
-    else:
-      print "Unexpected erosion level:", self._erosion_level
-      sys.exit(1)
-      return self._erosion_level
+    self._region_type = self._erosion_level % 3
+    return self._erosion_level
 
 
 class Cave(object):
@@ -57,24 +56,33 @@ class Cave(object):
     self.populate()
 
   def populate(self):
-    for y in range(0, self.target[0] + 5):
-      for x in range(0, self.target[1] + 5):
+    for y in range(0, self.target[1] + 7):
+      for x in range(0, self.target[0] + 7):
         self.regions[(x, y)] = Region(x, y, self.depth)
         if x > 0:
           self.regions[(x, y)].xminus1 = self.regions[(x - 1, y)]
         if y > 0:
           self.regions[(x, y)].yminus1 = self.regions[(x, y - 1)]
-    # TODO: fix this private access
-    self.regions[(0, 0)]._geologic_index = 0
-    self.regions[target]._geologic_index = 0
-    #for y in range(0, self.target[0] + 5):
-    #  for x in range(0, self.target[1] + 5):
+    self.regions[target].is_target = True
+
+  def risk_level(self, min_x, min_y, max_x, max_y):
+    risk = 0
+    for y in range(min_y, max_y + 1):
+      for x in range(min_x, max_x + 1):
+        risk += self.regions[(x, y)].region_type()
+    return risk
 
   def draw(self):
+    symbols = {
+      0: ".",  # rocky
+      1: "=",  # wet
+      2: "|",  # narrow
+    }
+
     s = ""
-    for y in range(0, self.target[0] + 5):
-      for x in range(0, self.target[1] + 5):
-        symbol = self.regions[(x, y)].region_type()
+    for y in range(0, self.target[1] + 6):
+      for x in range(0, self.target[0] + 6):
+        symbol = symbols[self.regions[(x, y)].region_type()]
         if (x, y) == target:
           s += "T"
         else:
@@ -83,9 +91,10 @@ class Cave(object):
     print s
 
 # main
-#depth = 6084
-#target = (14, 709)
 depth = 510
 target = (10, 10)
+depth = 6084
+target = (14, 709)
 cave = Cave(depth, target)
 cave.draw()
+print "Risk level is", cave.risk_level(0, 0, target[0], target[1])
