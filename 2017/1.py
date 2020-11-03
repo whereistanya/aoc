6bd9832f5c64b8e76d3ec3d1c3c0ae938c8eb4b2
Tanya Reilly <treilly@squarespace.com>
2020-11-02 21:18:24 -0500

    1

---
+++ 2017/1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Advent of code Day 1.
+
+import math
+
+def add(digits, part):
+  sum = 0
+  l = len(digits)
+  for i in range (0, l):
+    if part == 1:
+      compare = (i + 1) % l
+    elif part == 2:
+      compare = (i + (l / 2)) % l
+    if digits[i] == digits[compare]:
+      sum += int(digits[i])
+  return sum
+
+# Part 1
+assert add([1,1,2,2], 1) == 3
+assert add([9,1,2,1,2,1,2,9], 1) == 9
+
+# Part 2
+assert add([1,2,1,2], 2) == 6
+assert add([1,2,1,3,1,4,1,5], 2) == 4
+
+with open("input1.txt", "r") as f:
+  digits = list(f.read().strip())
+  print add(digits, 1)
+  print add(digits, 2)
+
+
