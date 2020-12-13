f66ef7c0be50e7e9a326351dabb0c8a9d40ba414
Tanya Reilly <treilly@squarespace.com>
2020-12-13 13:16:48 +0000

    Day 13 part 2.

--- 2020/13.kt
+++ 2020/13.kt
@@ -1,19 +1,15 @@
-// Advent of code 2020
+// Advent of code 2020 day 13. Far far from optimal.
 import java.io.File
 
 fun main() {
   val arrival = 1006726
   val input = "23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,647,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,x,x,x,x,x,x,29,x,557,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,17"
 
-  //val arrival = 939
-  //val input ="7,13,x,x,59,x,31,19"
-
   val buses: List<Int> = input.trim().split(",").filter {
       it != "x" }.map { it.toInt() }
 
   val timeMap = mutableMapOf<Int, Int>()
   for (bus in buses) {
-    println(bus)
     var i = 0
     while (true) {
       val minute = bus * i
@@ -25,6 +21,7 @@ fun main() {
     }
   }
 
+  // Part one.
   var i = arrival
   while (true) {
     if (timeMap.contains(i)) {
@@ -38,4 +35,60 @@ fun main() {
     }
     i += 1
   }
+
+  // Part two.
+  val part2: List<String> = input.trim().split(",")
+  val factors = mutableListOf<Int>()
+  // 647, the 23rd bus, is the biggest number, so anchor on that.
+  // This fact list is not used; it's just to print out for
+  // understanding how far away from 23 everything else is.
+  for (i in (0..(part2.size) - 1)) {
+    if (part2[i] == "x") {
+      factors.add(-1)
+    } else {
+      factors.add(part2[i].toInt())
+    }
+    println("${i - 23}, ${part2[i]}")
+  }
+  // println(factors)
+
+  val zero: Long = 0  // This can't be the best way to compare a long against
+                      // zero...
+  var j: Long = 647   // Could shave some minutes by starting at the first
+                      // multiple of 647 over 100000000000000 but whatevs.
+  // This code would be clearer if I took the numbers in order, but it's an
+  // order of magnitude fewer checks to test every 647th number rather than
+  // every 23rd one.
+  while (true) {
+    j += 647
+    if ((j + 31) % 557 != zero) {
+      continue
+    }
+    if ((j - 10) % 41 != zero) {
+      continue
+    }
+    if ((j + 37) % 37 != zero) {
+      continue
+    }
+    if ((j + 29) % 29 != zero) {
+      continue
+    }
+    if ((j - 23) % 23 != zero) {
+      continue
+    }
+    if ((j + 19) % 19 != zero) {
+      continue
+    }
+    if ((j + 48) % 17 != zero) {
+      continue
+    }
+    if ((j + 18) % 13 != zero) {
+      continue
+    }
+    println(j - 23) //      867295486378319, 29m later.
+    break
+    }
+  }
+  println("Part 2: $j - 23")
+
 }
