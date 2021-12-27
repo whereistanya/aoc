08a15201bc1555b1c96a1072d9baecd1ada062d6
Tanya Reilly <treilly@squarespace.com>
2021-12-27 07:49:44 -0700

    15

--- 2021/15.py
+++ 2021/15.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import os
+import collections
 
-inputfile = "input.txt"
+inputfile = "input"
 with open(inputfile, "r") as f:
   lines = [x.strip() for x in f.readlines()]
 
@@ -30,20 +31,21 @@ class color:
    END = '\033[0m'
 
 class Point(object):
-  def __init__(self, x, y, energy, grid):
+  def __init__(self, x, y, risk, grid):
+    self.name = "%s,%s" % (x, y)
     self.x = x
     self.y = y
-    self.energy = int(energy)
+    self.risk = int(risk)
     self.color = color.YELLOW
     self.grid = grid
 
-  def neighbours(self):
-    return self.grid.neighbours((self.x, self.y))
+  def neighbours(self, diagonal=True):
+    return self.grid.neighbours((self.x, self.y), diagonal)
 
 class Grid(object):
   def __init__(self, lines):
     self.lines = lines # [str, str, ...]
-    self.grid = {}  # (x, y): int
+    self.grid = {}  # (x, y): Point
     self.populate()
     self.height = len(lines)
     self.width = len(lines[0])
@@ -70,22 +72,74 @@ class Grid(object):
     for y in range(self.height):
       s = ""
       for x in range(self.width):
-        s += (self.grid[(x, y)].color + "%d" % self.grid[(x, y)].energy +
+        s += (self.grid[(x, y)].color + "%d" % self.grid[(x, y)].risk +
               color.END)
       print(s)
     print("\n")
 
-lines = test()
+#lines = test()
+
+# Part 2 messing...
+newlines = []
+for line in lines:
+  newline = ""
+  newline2 = ""
+  for i in range(0, 5):
+    for j in line:
+      new = int(j) + i
+      if new > 9:
+        new = (new % 10) + 1
+      newline += str(new)
+  newlines.append(newline)
+  print newline
+
+lines = []
+for i in range(0, 5):
+  for line in newlines:
+    newline = ""
+    for j in line:
+      new = int(j) + i
+      if new > 9:
+        new = (new % 10) + 1
+      newline += str(new)
+    lines.append(newline)
+
 grid = Grid(lines)
 
-# Part 1
+def dijkstra(grid, start, end):
+  # This is really, really slow.
+  visited = {start: 0}
 
-path = []
+  nodes = []
+  nodes = grid.grid.values() # [Point, ...]
+
+  while nodes:
+    min_node = None
+    for node in nodes:
+      if node in visited:
+        if min_node is None:
+          min_node = node
+        elif visited[node] < visited[min_node]:
+          min_node = node
+    if min_node is None:
+      break
+
+    nodes.remove(min_node)
+    current_weight = visited[min_node]
+
+    for edge in min_node.neighbours(diagonal=False):
+      weight = edge.risk + current_weight
+      if edge not in visited or weight < visited[edge]:
+        visited[edge] = weight
+  return visited[end]
+
 start = grid.grid[(0, 0)]
 start.color = color.GREEN
 end = grid.grid[(grid.width - 1, grid.height - 1)]
 end.color = color.RED
+end.name = "end"
+start.name = "start"
 grid.printgrid()
 
-def findpath():
-  pass
+print (dijkstra(grid, start, end))
+
