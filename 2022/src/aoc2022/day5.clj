e74ddec49aa1a6646def9c3f99b22b1472dfc55c
Tanya Reilly <treilly@squarespace.com>
2022-12-06 22:43:28 -0500

    Day 5 in Clojure

--- 2022/src/aoc2022/day5.clj
+++ 2022/src/aoc2022/day5.clj
@@ -2,8 +2,6 @@
   (:require [clojure.string :as str]))
   (use '[clojure.test :only [is]])
 
-
-
 (defn get-indexes [line]
   (let [m (map-indexed vector line)
         only-chars (filter #(Character/isUpperCase (second %)) m)
@@ -15,7 +13,7 @@
 
 (defn parse-move [s]
   (let [groups (re-matches #"move (\d+) from (\d+) to (\d+)" s)
-                how-many (second groups)
+                how-many (Integer/parseInt (second groups))
                 move-from (Integer/parseInt (nth groups 2))
                 move-to (Integer/parseInt (nth groups 3))]
     { :how-many how-many :from move-from :to move-to}
@@ -28,40 +26,45 @@
   (inc (* 4 (- i 1)))
 )
 
-(defn part1 [stacks moves]
-  "TODO"
-  (doseq [line stacks]
-    (println "LINE:" line))
-  ;(get-indexes (first stacks))
+(defn process-moves
+  "Apply each move one at a time"
+  [stacks moves part]
+  (loop [stacks stacks
+         moves moves
+         part part]
+    (let [[line & remaining-moves] moves
+          move (parse-move line)
+          real-from (real-index (get move :from))
+          real-to (real-index (get move :to))
+          how-many (get move :how-many)
+          after-removal (subvec (get stacks real-from) how-many)
+          to-add-p1 (vec (reverse (take how-many (get stacks real-from))))
+          to-add-p2 (vec (take how-many (get stacks real-from)))
+          to-add (if (= part :part1) to-add-p1 to-add-p2)
+          after-add (into to-add (get stacks real-to))
+          next-stacks (into stacks
+                        { real-from after-removal
+                          real-to after-add })
+          ]
+    (if (empty? remaining-moves)
+      next-stacks
+      (recur next-stacks remaining-moves part)
+      )
+    )
+  )
+)
+
+(defn move-crates [stacks moves part]
+  "Move crates around."
   (let [
     indexes
       (for [line stacks
            :let [found (get-indexes line)] ]
            found )
      columns (apply merge-with into indexes)
-   ]
-
-    (doseq [line columns]
-      (println "MUNGED:" line))
-    ;(println (str/index-of keyline \3))
-  (doseq [line moves]
-    (let [move (parse-move line)
-          real-from (real-index (get move :from))
-          real-to (real-index (get move :to))
-          how-much (get move :how-many)
-          ]
-      (println line)
-      (println move "=>" how-much real-from real-to)
-
-    )
-   )
-  )
-)
-
-(defn part2 [s]
-  "TODO"
-  ( ->> (str/split s #"\n")
-        (map str/trim)
+     final-stacks (process-moves columns moves part)
+     ]
+     (apply str (map first (vals(sort final-stacks))))
   )
 )
 
@@ -74,17 +77,15 @@
 (defn main
   [& args]
 
-  (let [s (slurp "src/aoc2022/test5.txt")
+  (let [s (slurp "src/aoc2022/input5.txt")
          [stacks moves] (map parse (str/split s #"\n\n"))
-         part1-result (part1 stacks moves)
-   ;     part2-result (part2 s)
+         part1-result (move-crates stacks moves :part1)
+         part2-result (move-crates stacks moves :part2)
         ]
 
-   (println "Part 1" part1-result)
-   ;(println "Part 2" part2-result)
-   ;(println stacks)
-   ;(println)
-   ;(println (first moves))
-
-   )
+    (doseq [line stacks]
+      (println "=:" line))
+    (println "Part 1" part1-result)
+    (println "Part 2" part2-result)
+  )
 )
