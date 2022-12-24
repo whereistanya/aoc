621025006e99fa5d2128ad495564e9b3446af99a
Tanya Reilly <treilly@squarespace.com>
2022-12-24 15:26:06 -0500

    Various grid improvements

--- 2021/grid.py
+++ 2021/grid.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+# TODO: rewrite with defaultdict
+
 class color:
    PURPLE = '\033[95m'
    CYAN = '\033[96m'
@@ -18,15 +21,22 @@ class Point(object):
     self.color = color.GREEN
     self.grid = grid
 
-  def neighbours(self, diagonal=True, default_value=""):
-    return self.grid.neighbours(self, diagonal=diagonal,
-                                default_value=default_value)
+  def neighbours(self, diagonal=True, default_value="", prune=False):
+    nabes = self.grid.neighbours(self, diagonal=diagonal,
+                                 default_value=default_value)
+    if prune:
+      nabes = [x for x in nabes if x]
+    return nabes
+
+  def neighbours_by_direction(self):
+    return self.grid.neighbours_by_direction(self)
 
   def __repr__(self):
     s = "Point(%d,%d)[%s]" % (self.x, self.y, self.value)
     return s
 
 class Grid(object):
+  """x increases to the east/right, y increases to the south/down"""
   def __init__(self, lines):
     self.lines = lines # [str, str, ...]
     self.grid = {}  # (x, y): Point
@@ -37,36 +47,108 @@ class Grid(object):
     self.maxy = 0
     self.populate()
 
+  def nw_xy(self, point):
+    return (point.x - 1, point.y - 1)
+
+  def n_xy(self, point):
+    return (point.x, point.y - 1)
+
+  def ne_xy(self, point):
+    return (point.x + 1, point.y - 1)
+
+  def e_xy(self, point):
+    return (point.x + 1, point.y)
+
+  def w_xy(self, point):
+    return (point.x - 1, point.y)
+
+  def sw_xy(self, point):
+    return(point.x - 1, point.y + 1)
+
+  def s_xy(self, point):
+    return (point.x, point.y + 1)
+
+  def se_xy(self, point):
+    return(point.x + 1, point.y + 1)
+
   def neighbours(self, point, diagonal=True, default_value=""):
     #print("Getting neighbours for %d,%d" % (point.x,point.y))
-    x = point.x
-    y = point.y
     neighbours = []
     if diagonal:
-      to_add = [(x - 1, y - 1), (x  , y - 1), (x + 1, y - 1),
-                (x - 1, y    ), (x  , y    ), (x + 1, y    ),
-                (x - 1, y + 1), (x  , y + 1), (x + 1, y + 1)]
+      # order is:
+      # nw, n, ne, w, self, e, sw, s, se
+      to_add = [self.nw_xy(point), self.n_xy(point),  self.ne_xy(point),
+                self.w_xy(point),  (point.x, point.y),self.e_xy(point),
+                self.sw_xy(point), self.s_xy(point),  self.s_xy(point)]
     else:
-      to_add = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+      to_add = [self.e_xy(point), self.w_xy(point),
+                self.s_xy(point), self.n_xy(point)]
     for neighbour in to_add:
       if neighbour in self.grid:
         neighbours.append(self.grid[neighbour])
-        self.grid[neighbour].color = color.DARKCYAN
       else:
         neighbours.append(None)
       #  if default_value != "":
       #    print("Neighbour %d,%d doesn't exist yet. Creating it." %
       #          (neighbour[0], neighbour[1]))
-      #    new_point = self.addpoint(neighbour[0], neighbour[1], default_value)
+      #    new_point = self.create_point(neighbour[0], neighbour[1], default_value)
       #    neighbours.append(new_point)
       #    new_point = color.YELLOW
     return neighbours
 
-  def addpoint(self, x, y, value):
+  def neighbours_by_direction(self, point, diagonal=True):
+    fns = {
+      "northwest": self.nw_xy,
+      "north": self.n_xy,
+      "northeast": self.ne_xy,
+      "west": self.w_xy,
+      "east": self.e_xy,
+      "southwest": self.sw_xy,
+      "south": self.s_xy,
+      "southeast": self.se_xy,
+    }
+    neighbours = {}
+    for direction, fn in fns.items():
+      p = fn(point)
+      if p in self.grid:
+        neighbours[direction] = self.grid[p]
+      else:
+        neighbours[direction] = None
+    return neighbours
+
+
+    if diagonal:
+      # order is:
+      # nw, n, ne, w, self, e, sw, s, se
+      to_add = [self.nw_xy(point), self.n_xy(point),  self.ne_xy(point),
+                self.w_xy(point),  (point.x, point.y),self.e_xy(point),
+                self.sw_xy(point), self.s_xy(point),  self.s_xy(point)]
+    else:
+      to_add = [self.e_xy(point), self.w_xy(point),
+                self.s_xy(point), self.n_xy(point)]
+    for neighbour in to_add:
+      if neighbour in self.grid:
+        neighbours.append(self.grid[neighbour])
+      else:
+        neighbours.append(None)
+      #  if default_value != "":
+      #    print("Neighbour %d,%d doesn't exist yet. Creating it." %
+      #          (neighbour[0], neighbour[1]))
+      #    new_point = self.create_point(neighbour[0], neighbour[1], default_value)
+      #    neighbours.append(new_point)
+      #    new_point = color.YELLOW
+    return neighbours
+
+  def create_point(self, x, y, value):
     #print("Creating a new point at %d,%d" % (x, y))
     new_point = Point(x, y, value, self)
     new_point.color = color.YELLOW
-    self.grid[(x, y)] = new_point
+    return self.addpoint(new_point)
+
+  def addpoint(self, point):
+    x = point.x
+    y = point.y
+    self.grid[(x, y)] = point
     if x >= self.maxx:
       self.maxx = x + 1
     if x < self.minx:
@@ -75,21 +157,24 @@ class Grid(object):
       self.maxy = y + 1
     if y < self.miny:
       self.miny = y
-    return new_point
+    return point
 
   def setvalue(self, x, y, value):
     #print("setvalue %d, %d, %s" % (x, y, value))
     if (x, y) in self.grid:
       self.grid[(x, y)].value = value
     else:
-      self.addpoint(x, y, value)
+      self.create_point(x, y, value)
+
+  def get_by_char(self, char):
+    return [v for k, v in self.grid.items() if v.value == char]
 
   def getpoint(self, x, y, default_value=None):
     #print("getpoint  %d,%d" % (x, y))
     if (x, y) in self.grid:
       return self.grid[(x, y)]
     elif default_value:
-      point = self.addpoint(x, y, default_value)
+      point = self.create_point(x, y, default_value)
       return point
     return None
 
