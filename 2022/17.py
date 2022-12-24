34f2209fbcbc2b20702faa3d2fe90dd2c109185b
Tanya Reilly <treilly@squarespace.com>
2022-12-23 23:22:55 -0500

    Day 17.

--- 2022/17.py
+++ 2022/17.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import math
+from collections import Counter
 
 filename = "test17.txt"
 filename = "input17.txt"
@@ -18,6 +19,8 @@ class Game(object):
     self.floor = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0)]
     self.grid.update(self.floor)
     self.tower_height = 0
+    self.additions = []
+    self.heights_at = {}
 
   def print(self):
     print("\n\n")
@@ -92,8 +95,11 @@ class Game(object):
         # stop moving
         self.grid.update(self.current)
         high_point = max([y for (x, y) in self.current])
+        self.additions.append(high_point - self.tower_height)
         if high_point > self.tower_height:
           self.tower_height = high_point
+        self.heights_at[i] = self.tower_height
+
         self.height = self.tower_height + 4
         break
       self.current = next_pos
@@ -108,8 +114,70 @@ entry = 2 # two units away from the left wall
 
 
 game = Game(data)
-for i in range (2022):
-  game.drop_rock()
-  if ((i + 1) % 5 == 0):
-    print(i + 1, game.tower_height)
-print(i + 1, game.tower_height)
+n = 10000
+for i in range (n):
+  game.drop_rock(i)
+  #if ((i + 1) % 5 == 0):
+  #  print(i + 1, game.tower_height)
+print("Part 1", i + 1, game.tower_height)
+
+# Find the cycle
+cycle_length = -1
+last_seen = -1
+dists = []
+
+# Get the least common number in the sequence and see if it repeats
+# if not, try the second least common, etc.
+most_common = Counter(game.additions).most_common()
+for i in range(-1, -1 * (len(most_common)), -1):
+  test, count = most_common[i]
+  if count <= 1:
+    continue
+  for i in range (len(game.additions)):
+    g = game.additions[i]
+    if g == test:
+      dists.append(i - last_seen)
+      last_seen = i
+  # Make sure all the distances between the appearance of the number have
+  # the same value. Skip the first one because there's probably some time
+  # before the repeat starts.
+  if all(dists[1] == x for x in dists[1:]):
+    cycle_length = dists[1]
+    break  # because we found it.
+
+if cycle_length == -1:
+  print("BUG: never found a cycle length")
+  exit(1)
+
+# Now figure out when the cycle begins. There are a lot of non-repeated
+# drops at the start before we start repeating.
+repeats_at = -1
+for i in range(0, cycle_length):
+  if (game.additions[i: i + cycle_length] ==
+      game.additions[i + cycle_length: i + cycle_length + cycle_length]):
+    repeats_at = i
+    break
+
+if repeats_at == -1:
+  print("BUG: never found a repeat")
+  exit(1)
+
+# Get the height change added each cycle.
+cycle_added_height = (game.heights_at[(repeats_at + cycle_length)] -
+                      game.heights_at[repeats_at])
+
+# Double check it's right.
+assert(game.heights_at[(repeats_at + cycle_length + cycle_length)] -
+       game.heights_at[(repeats_at + cycle_length)] == cycle_added_height)
+
+print ("Starts repeating after", repeats_at)
+print("Repeats every %d drops" % cycle_length)
+print("Every cycle we add", cycle_added_height)
+
+n = 1000000000000
+# Finally, figure out how many cycles we need to run, and how much extra
+# to add (the offset) to get to exactly n cycles.
+cycle_count = math.floor((n - repeats_at) / cycle_length)
+offset = n - ((cycle_count * cycle_length) + repeats_at + 1)
+print("Part 2:", game.heights_at[repeats_at + offset] +
+                 (cycle_count * cycle_added_height))
