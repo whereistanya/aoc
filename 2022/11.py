4823b0490159760f1be020613e9de1370085de5f
Tanya Reilly <treilly@squarespace.com>
2022-12-26 21:13:31 -0500

    11 part 2

--- 2022/11.py
+++ 2022/11.py
@@ -6,8 +6,14 @@ import math
 import operator
 import re
 
-filename = "input11.txt"
-#filename = "test11.txt"
+test = True
+test = False
+
+if test:
+  filename = "test11.txt"
+else:
+  filename = "input11.txt"
+
 with open(filename, "r") as f:
   lines = [x.strip() for x in f.readlines()]
 
@@ -21,6 +27,11 @@ class Monkey(object):
     self.trueto = None
     self.falseto = None
     self.throwcount = 0
+    self.monkeys = []
+    if test:
+      self.magic = 23 * 19 * 13 * 17
+    else:
+      self.magic = 17 * 2 * 5 * 3 * 7 * 13 * 19 * 11
 
   def add_items(self, items):
     self.has.extend(items)
@@ -49,61 +60,62 @@ class Monkey(object):
 
   def throw(self, part=1):
     "Inspect the item, choose where to throw it."
-    item = self.has.popleft()
+
+    orig = self.has.popleft()
     self.throwcount += 1
-    item = self.operate(item)
+    item = self.operate(orig)
     if part == 1:
       item = int(math.floor(item / 3.0))
-    #if part == 2:
-      # we want to know if the item's divisible by 23, 19, 13 or 17
+    else:
+      item = item % self.magic
 
     if item % self.div == 0:
-      return item, self.trueto
+      giveto = self.monkeys[self.trueto]
     else:
-      return item, self.falseto
+      giveto = self.monkeys[self.falseto]
+
+    giveto.has.append(item)
 
   def __repr__(self):
     return "Monkey %s, holding %s" % (self.name, self.has)
-
-print (lines)
 
 monkeys = {}
 
 current = None
 
-for line in lines:
-  if line == "":
-    continue
-  if line.startswith("Monkey"):
-    name = current = int(line.split("Monkey ")[1].split(":")[0])
-    current = Monkey()
-    current.name = name
-    monkeys[name] = current
-  elif line.startswith("Starting items:"):
-    current.add_items(
-      [int(x.strip()) for x in line.split("Starting items: ")[1].split(",")])
-  elif line.startswith("Operation:"):
-    current.set_fn(line.split("Operation: new = ")[1])
-  elif line.startswith("Test:"):
-    current.div = int(line.split("Test: divisible by ")[1])
-  elif line.startswith("If true:"):
-    current.trueto = int(line.split("If true: throw to monkey ")[1])
-  elif line.startswith("If false:"):
-    current.falseto = int(line.split("If false: throw to monkey ")[1])
-  else:
-    print("PARSE ERROR: got", line)
-    exit(1)
+def parse(lines):
+  monkeys = {}
+  for line in lines:
+    if line == "":
+      continue
+    if line.startswith("Monkey"):
+      name = current = int(line.split("Monkey ")[1].split(":")[0])
+      current = Monkey()
+      current.name = name
+      current.monkeys = monkeys
+      monkeys[name] = current
+    elif line.startswith("Starting items:"):
+      current.add_items(
+        [int(x.strip()) for x in line.split("Starting items: ")[1].split(",")])
+    elif line.startswith("Operation:"):
+      current.set_fn(line.split("Operation: new = ")[1])
+    elif line.startswith("Test:"):
+      current.div = int(line.split("Test: divisible by ")[1])
+    elif line.startswith("If true:"):
+      current.trueto = int(line.split("If true: throw to monkey ")[1])
+    elif line.startswith("If false:"):
+      current.falseto = int(line.split("If false: throw to monkey ")[1])
+    else:
+      print("PARSE ERROR: got", line)
+      exit(1)
+  return monkeys
 
 # Part 1
+monkeys = parse(lines)
 for round in range (1, 21):
   for i in range(len(monkeys)):
     while monkeys[i].has_items():
-      item, who = monkeys[i].throw()
-      #print ("Monkey %d throws %d to monkey %d" % (i, item, who))
-      monkeys[who].has.append(item)
-  #print("* After round", round)
-  #for m in monkeys:
-  #  print (monkeys[m], monkeys[m].throwcount)
+      monkeys[i].throw()
 
 counts = {}
 for m in monkeys:
@@ -113,19 +125,12 @@ for m in monkeys:
 actives = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
 print ("Part 1", actives[0][1] * actives[1][1])
 
-exit(0)
-
 # Part 2
-# TODO: math magic required
+monkeys = parse(lines)
 for round in range (1, 10001):
   for i in range(len(monkeys)):
     while monkeys[i].has_items():
-      item, who = monkeys[i].throw(part=2)
-      #print ("Monkey %d throws %d to monkey %d" % (i, item, who))
-      monkeys[who].has.append(item)
-  print("* After round", round)
-  for m in monkeys:
-    print (m, monkeys[m].has)
+      monkeys[i].throw(part=2)
 
 counts = {}
 for m in monkeys:
@@ -133,10 +138,5 @@ for m in monkeys:
   counts[m] = monkey.throwcount
 
 actives = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
-print (actives[0][1] * actives[1][1])
+print ("Part 2", actives[0][1] * actives[1][1])
 
-#
-
-# 13223735010 too high
-# 2950025208 too low
-# 2713310158 
