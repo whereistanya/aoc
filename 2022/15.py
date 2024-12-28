7c53bd12fec1bd9439243bcac4ef8830c1a3a099
Tanya Reilly <tanya@noidea.dog>
2024-12-28 00:30:01 -0700

    2022 day 15

--- 2022/15.py
+++ 2022/15.py
@@ -1,139 +1,107 @@
 #!/usr/bin/env python3
 
-import math
 import re
 
 test = False
-test = True
+#test = True
 
 if test:
   filename = "test15.txt"
+  part1Row = 10
+  minxy = 0
+  maxxy = 20
 else:
   filename = "input15.txt"
+  part1Row = 2000000
+  minxy = 0
+  maxxy = 4000000
 
 with open(filename, "r") as f:
   lines = [x.strip() for x in f.readlines()]
 
-empty = set()
-sensors = set()
+class Sensor(object):
+  def __init__(self, x, y, bx, by):
+    self.x = x
+    self.y = y
+    self.bx = bx
+    self.by = by
+    # the radius from the sensor. no beacon is nearer to the sensor than this value
+    self.space = abs(bx - x) + abs(by - y)
+
+  def in_sensor_range(self, y):
+    ydist = abs(self.y - y)
+    remaining = self.space - ydist
+    if remaining < 0: # this row is too far away to be in this sensor's diamond
+      return 0,0,False
+
+    a = self.x - remaining
+    b = self.x + remaining
+    return min(a,b), max(a, b), True
+
+sensors = []
 beacons = set()
-empty_range = {}
 
-def closer(point, start, max_distance):
-  "Return True if point is within distance of start, False otherwise"
-  sx, sy = start
-  px, py = point
-  x_dist = abs(sx - px)
-  y_dist = abs(sy - py)
-  dist = x_dist + y_dist
-  # TODO: which branch is = on?
-  if dist <= max_distance:
-    return True
-  else:
-    return False
-
-min_x = math.inf
-max_x = -math.inf
-
+# Parse input. It looks like:
+#lines = ["Sensor at x=8, y=7: closest beacon is at x=2, y=10"]
 line_re = "Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)"
 for line in lines:
   matches = re.finditer(line_re, line)
   for match in matches:
     sx, sy, bx, by = [int(x) for x in match.groups()]
-    sensors.add((sx, sy))
-    beacons.add((bx, by))
-# Uncomment these lines to only draw empty space for sensor 8,7
-# like in the example, to compare pictures.
-#lines = ["Sensor at x=8, y=7: closest beacon is at x=2, y=10"]
-#for line in lines:
-#  matches = re.finditer(line_re, line)
-#  for match in matches:
-#    sx, sy, bx, by = [int(x) for x in match.groups()]
+    sensors.append(Sensor(sx, sy, bx, by))
+    beacons.add((bx,by))
 
-    x_dist = abs(sx - bx)
-    y_dist = abs(sy - by)
-    dist = x_dist + y_dist
-    print (line)
-    print(sx, sy, bx, by, dist)
-    if (sx - dist) < min_x:
-      min_x = (sx - dist)
-    if (sx + dist) > max_x:
-      max_x = (sx + dist)
-    empty_range[(sx, sy)] = dist
 
-    """for x in range(dist + 1):
-      for y in range(0, (dist - x + 1)):
-        empty.add((sx + x, sy + y))
-        empty.add((sx - x, sy + y))
-        empty.add((sx + x, sy - y))
-        empty.add((sx - x, sy - y))
-print("  --          1111111111122222")
-print("  2101234567890123456789012345")
-for y in range (-2, 23):
-  s = str(y).zfill(2)
-  for x in range (-2, 26):
-    if (x, y) in sensors:
-      s += "S"
-    elif (x, y) in beacons:
-      s += "B"
-    elif (x, y) in empty:
-      s += "#"
-    else:
-      s += "."
-  print (s)
-"""
+def findgaps(row, sensors):
+  ranges = []
+  for s in sensors:
+    smin, smax, in_range = s.in_sensor_range(row)
+    if not in_range:
+      continue
+    ranges.append((smin, smax))
 
-"""
+  ranges = sorted(ranges) # (minx, maxx) for a given row, y
+  gaps = [] # range is inclusive
+
+  start = ranges[0][0]
+  end = 0
+  index = start
+
+  for rmin, rmax in ranges:
+    if index >= rmin and index <= rmax:
+      index = rmax + 1
+    elif index < rmin:
+      gaps.append((index, rmin - 1))
+      index = rmax
+    if rmax > end:
+      end = rmax
+    # don't check if it's greater than rmax because we might have gone past the end
+    # of rmax and into the next range.
+  return gaps, start, end
+
+
 # Part 1
-print ("Looking from %d to %d" % (min_x, max_x))
-count = 0
+# Each sensor lists closest beacon, showing where there can't be beacons
+gaps, start, end = findgaps(part1Row, sensors)
+impossible = end - start
+print("Part 1:", impossible)
+for rmin, rmax in gaps: # all ranges not covered by sensors
+  print("minus %d-%d inclusive" % (rmin, rmax))
 
 
-if test:
-  row = 10
-else:
-  row = 2000000
-min_y = row
-max_y = row
-for y in range(min_y, max_y + 1):
-  for x in range (min_x, max_x + 1):
-    if (x, y) in beacons:
-      continue
-    for sensor in sensors:
-      if closer((x, y), sensor, empty_range[sensor]):
-        count += 1
-        break
-print("Part 1", count)
+# Part2
+# Find the only possible position for the distress beacon.
+part2 = 0
+for row in range(0, maxxy):
+  print(row)
+  gaps, start, end = findgaps(row, sensors)
+  #print(gaps, start, end)
+  if gaps:
+    print("Found gaps on row %d: %s" % (row, gaps))
+    x = gaps[0][0]
+    y = row
+    print("x=%d, y=%d" % (x, y))
+    part2 = x * 4000000 + y
+    break
 
-exit(0)
-"""
-
-# Part 2
-min_x = 0
-min_y = 0
-
-if test:
-  max_x = 20
-  max_y = 20
-else:
-  max_x = 4000000
-  max_y = 4000000
-
-print ("Looking from %d to %d" % (min_x, max_x))
-for y in range(min_y, max_y + 1):
-  print(y)
-  for x in range (min_x, max_x + 1):
-    if (x, y) in beacons:
-      continue
-    if (x, y) in sensors:
-      continue
-    found = False
-    for sensor in sensors:
-      if closer((x, y), sensor, empty_range[sensor]):
-        found = True
-        break
-    if not found:
-      print("%d,%d is possible!" % (x, y))
-      print("Freq", (x * 4000000 + y))
-      exit(0)
-
+print("Part 2:", part2)
